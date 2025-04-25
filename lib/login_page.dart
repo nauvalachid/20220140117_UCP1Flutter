1809037213191f3c81b34c9ad2db363cf7e6aa53
nauvalachid <nauvalachid@gmail.com>
2025-04-25 14:02:49 +0700

    Menambahkan kelas status baru _LoginPageState dengan pengontrol email dan kata sandi

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,3 +6,13 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+   @override
+  void initState() {
+    super.initState();
+  }
+}
