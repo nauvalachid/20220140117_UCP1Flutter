392afbe9053e566a61b0902a0043f7f92e1766cb
nauvalachid <nauvalachid@gmail.com>
2025-04-25 15:14:13 +0700

    Menambahkan fungsi halaman login ke aplikasi

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,4 +15,85 @@ class _LoginPageState extends State<LoginPage> {
   void initState() {
     super.initState();
   }
+
+ @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'SELAMAT DATANG KEMBALI',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+               const Text(
+                'Email',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+               SizedBox(height: 16),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email',border: OutlineInputBorder(),prefixIcon: Icon(Icons.email)),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+                SizedBox(height: 16),
+                 const Text(
+                'Password',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+                SizedBox(height: 16),
+               TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password',border: OutlineInputBorder(),prefixIcon: Icon(Icons.lock)),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text('Login'),
+                                 ),
+                 ],
+               ),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   TextButton(
+                    onPressed: () {
+                    },
+                    child: Text('Belum punya akun? Daftar'),
+                                 ),
+                 ],
+               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
