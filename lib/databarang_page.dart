c6e63340f2d39e25240286c94f86c656f8dea45b
nauvalachid <nauvalachid@gmail.com>
2025-04-29 20:52:35 +0700

    Menambahkan widget DataBarangPage dengan struktur dan bidang dasar

--- lib/databarang_page.dart
+++ lib/databarang_page.dart
@@ -1,2 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class DataBarangPage extends StatefulWidget {
+  const DataBarangPage({super.key});
+
+  @override
+  State<DataBarangPage> createState() => _DataBarangPageState();
+}
+
+class _DataBarangPageState extends State<DataBarangPage> {
+  final TextEditingController jenisbarangController = TextEditingController();
+  final TextEditingController tanggalController = TextEditingController();
+  final TextEditingController jenistransaksiController = TextEditingController();
+  final TextEditingController jumlahController = TextEditingController();
+  final TextEditingController hargaController = TextEditingController();
+
+  DateTime? _selectedDate;
+  bool _isDateSelected = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
