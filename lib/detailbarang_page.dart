f31bd56461953c14d94b97ba0f7d3db27343fc34
nauvalachid <nauvalachid@gmail.com>
2025-04-29 22:42:14 +0700

    Menambahkan widget DetailPiketPage

--- lib/detailbarang_page.dart
+++ lib/detailbarang_page.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String tanggal;
+  final String jenistransaksi;
+  final String jenisbarang;
+  final String jumlahbarang;
+  final String jenisharga;
+  final String totalharga;
+}
