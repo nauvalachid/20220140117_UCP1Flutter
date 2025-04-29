56085d36ad9961f3a313acbc4baa3ec720d46de8
nauvalachid <nauvalachid@gmail.com>
2025-04-29 20:29:52 +0700

    Menambahkan kolom nama, email, dan noHP ke DetailPelangganPage dan memperbarui UI untuk menampilkan kolom tersebut.

--- lib/detailpelanggan_page.dart
+++ lib/detailpelanggan_page.dart
@@ -4,19 +4,25 @@ class DetailPelangganPage extends StatelessWidget {
   final String alamat;
   final String provinsi;
   final String kodepos;
+  final String nama;
+  final String email;
+  final String noHP;
 
     const DetailPelangganPage({
     Key? key,
     required this.alamat,
     required this.provinsi,
     required this.kodepos,
+    required this.nama,
+    required this.email,
+    required this.noHP,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail Pelanggan',
+        title: Text('Detail ${nama}',
           style: const TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
@@ -30,6 +36,82 @@ class DetailPelangganPage extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
         ),
       ),
+       body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/pp.jpeg'), // Ganti sesuai path asetmu
+            ),
+            const SizedBox(height: 16),
+            Text(
+              nama,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Text(email),
+            Text(noHP),
+            const SizedBox(height: 32),
+            buildField('Alamat', alamat),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(child: buildField('Provinsi', provinsi)),
+                const SizedBox(width: 16),
+                Expanded(child: buildField('Kode Pos', kodepos)),
+              ],
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: double.infinity,
+              height: 55,
+              child: ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFE75027),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  'Selesai',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+Widget buildField(String label, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          initialValue: value,
+          enabled: false,
+          style: const TextStyle(color: Colors.black), 
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
