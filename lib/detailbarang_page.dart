98c3cfb06cf26ffd0479b438da7c59d208d25afe
nauvalachid <nauvalachid@gmail.com>
2025-04-29 22:44:32 +0700

    Menambahkan fungsi untuk memformat tanggal di DetailBarangPage

--- lib/detailbarang_page.dart
+++ lib/detailbarang_page.dart
@@ -18,4 +18,14 @@ class DetailBarangPage extends StatelessWidget {
     required this.jenisharga,
     required this.totalharga,
   }) : super(key: key);
+
+   // Fungsi untuk mengubah format tanggal
+  String formatTanggal(String tanggalAsli) {
+    try {
+      DateTime parsedDate = DateFormat('dd-MM-yyyy').parse(tanggalAsli);
+      return DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(parsedDate);
+    } catch (e) {
+      return tanggalAsli;
+    }
+  }
 }
