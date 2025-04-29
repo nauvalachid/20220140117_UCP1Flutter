b7bba62e55bdb8edaf820e39710cfdd5b7a15cbb
nauvalachid <nauvalachid@gmail.com>
2025-04-29 22:43:48 +0700

    Refaktor DetailPiketPage menjadi DetailBarangPage

--- lib/detailbarang_page.dart
+++ lib/detailbarang_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class DetailPiketPage extends StatelessWidget {
+class DetailBarangPage extends StatelessWidget {
   final String tanggal;
   final String jenistransaksi;
   final String jenisbarang;
   final String jumlahbarang;
   final String jenisharga;
   final String totalharga;
+
+   const DetailBarangPage({
+    Key? key,
+    required this.tanggal,
+    required this.jenistransaksi,
+    required this.jenisbarang,
+    required this.jumlahbarang,
+    required this.jenisharga,
+    required this.totalharga,
+  }) : super(key: key);
 }
